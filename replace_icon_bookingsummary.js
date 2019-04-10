d9eaf1feb5ad0351816aaeff77ddd2c47197a5ed
uadevdo <49495833+uadevdo@users.noreply.github.com>
2019-04-10 15:46:20 -0400

    Update replace_icon_bookingsummary.js

--- replace_icon_bookingsummary.js
+++ replace_icon_bookingsummary.js
@@ -1,3 +1,3 @@
-jQuery_3_3_1(document).ready(function(){
-	jQuery_3_3_1('i.am-locations:contains("2019")').addClass("timepiece");
+$(document).ready(function(){
+	$('i.am-locations:contains("2019")').addClass("timepiece");
 });
