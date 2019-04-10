ddbd436048ca3d6729b384a9db8e57066abe13c6
uadevdo <49495833+uadevdo@users.noreply.github.com>
2019-04-10 15:01:31 -0400

    Update replace_icon_bookingsummary.js

--- replace_icon_bookingsummary.js
+++ replace_icon_bookingsummary.js
@@ -1,7 +1,3 @@
 $(document).ready(function(){
-	$("i.am-locations:contains(\"2019\")").addClass("timepiece");
+	$('i.am-locations:contains("2019")').addClass("timepiece");
 });
-
-// $("body").append("<script src='https://www.ualibrary.org/assets/communicojs/replace_icon_bookingsummary.js' type='text/javascript'><\/script>");
-
-$(function(){("#body").append("<script>$(function(){('i.am-locations:contains('2019')').addClass('timepiece');});</script>");});
